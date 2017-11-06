5fa56ae939a78c43c10918c4af4d52271f66fe30
wllynilson <wllynilson@gmail.com>
2017-11-06 12:33:14 -0200

    Padronização do cabeçalho

--- Linguagem Natural/Linguagem Natural/ProcessarLinguagem.cs
+++ Linguagem Natural/Linguagem Natural/ProcessarLinguagem.cs
@@ -13,16 +13,18 @@ namespace Linguagem_Natural
     {
         static void Main(string[] args)
         {
-            //   Cabecalho();
+               Cabecalho();
             LerArquivoJson("conversas.json");
             LerArquivoJson("produtos.json");
             LerArquivoJson("acoes.json");
+
+            ProcessaEstatisticas("conversas.json","acoes.json");
         }
         static void Cabecalho()
         {
             JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-            string json = @"{ ""nome"" : ""Wllynilson"", ""sobrenome"" : ""Carneiro"", ""email"": ""wllynilson@gmail.com"" }";
-            //string json2 =@"{ ""nome"" : ""Teste"", ""sobrenome"" : ""Outro Teste"", ""email"": ""teste @gmail.com""}";
+            string json = @"{ ""Disciplina"" : ""Linguagens Formais"", ""Professor"" : ""Jackson Gomes"", ""Alunos"": ""[Ewerton Santiago, Marcos Mourão, Maria do Carmo, Wllynilson Carneiro]"" }";
+            
 
             dynamic resultado = serializer.DeserializeObject(json);
             Console.WriteLine("-> -> Resultado da leitura do arquivo JSON <- <-");
@@ -56,15 +58,32 @@ namespace Linguagem_Natural
             }
         }
 
-        static void ProcessaEstatisticas(string dialogo, string expressao)
+        static void ProcessaEstatisticas(string dialogo, string acoes)
         {
             //string expressao = "";
             //nova instancia de Regex
-            var rgx = new Regex("");
+            var rgx = new Regex(@"([Oo]i, eu sou [?oa] [A-Za-z]*)|([Oo]i, sou o [A-Za-z]*)|([Ee]u sou o [A-Za-z]*)|(sou [o|a] [A-Za-z]*)+(. Gostaria de )|(e queria)|(\w+)");
+            var actions = new Regex(@"(comprar)|(saber)|(vender)|(gostaria)|(desejo)|(valor)|(pagamento)");
+
+            //adiciona o arquivo 'acoes' para começar a processa-lo
+            var arquivo = rgx.Matches(acoes);
+
+            //novo dicionário com 'chave valor'
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+            //iteração sobre o arquivo percorrendo palavra por palavra
+            foreach(Match item in arquivo)
+            {
+                dic.Add(int.Parse(item.Groups[0].Value), item.Groups[1].Value);
+                Console.WriteLine(dic);                
+            }
 
 
             Match m = rgx.Match(dialogo);
-
+            Console.WriteLine(m);
+            Console.ReadKey();
+            //Console.WriteLine(rgx);
+            Console.ReadKey();
         }
     }
 }
